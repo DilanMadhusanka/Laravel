bc841cd05adc5c5c05aae0684bf991bf4c6a2953
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-11-07 22:09:00 +0530

    creating a web form

--- pizzahouse/resources/views/pizzas/create.blade.php
+++ pizzahouse/resources/views/pizzas/create.blade.php
@@ -1,10 +1,25 @@
 @extends('layouts.layout')
 @section('content')
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-        <div class="content">
-            <div class="title m-b-md">
-                Add a New Pizza
-            </div>
-        </div>
+    <div class="wrapper create-pizza">
+        <h1>Create a New Pizza</h1>
+        <form action="" method="">
+            <label for="name">Your name</label>
+            <input type="text" id="name" name="name">
+            <label for="type">Choose pizza type:</label>
+            <select name="type" id="type">
+                <option value="margarita">Margarita</option>
+                <option value="hawaiian">Hawaiian</option>
+                <option value="veg supreme">Veg Supreme</option>
+                <option value="volcano">Volcano</option>
+            </select>
+            <label for="base">Choose base type:</label>
+            <select name="base" id="base">
+                <option value="cheesy crust">Cheesy Crust</option>
+                <option value="garlic crust">Garlic Crust</option>
+                <option value="thin & crispy">Thin & Crispy</option>
+                <option value="thick">Thick</option>
+            </select>
+            <input type="submit" value="Order Pizza">
+        </form>
     </div>
 @endsection
