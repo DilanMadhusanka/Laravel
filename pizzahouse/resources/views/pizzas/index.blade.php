2d4aa8839659724ec17fec9867ff01fcb6b2d0e5
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-11-08 16:08:45 +0530

    authentication

--- pizzahouse/resources/views/pizzas/index.blade.php
+++ pizzahouse/resources/views/pizzas/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.layout')
+@extends('layouts.app')
 @section('content')
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
         <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
